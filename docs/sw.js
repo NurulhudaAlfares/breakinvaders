47899ac71d942c2ec50c0a5b2093571000d0073e
DESKTOP-RD4UJSQ\n-a77 <nurulhuda.alfares@gmail.com>
2025-05-21 21:22:08 +0300

    docs: update documentation with touch controls and version 1.1.1

--- docs/sw.js
+++ docs/sw.js
@@ -4,45 +4,38 @@ const ASSETS_TO_CACHE = [
     './index.html',
     './game.js',
     './manifest.json',
-    './favicon.png',
-    './img/bg.png',
-    './img/spaceship.png',
-    './img/bullet.png',
-    './img/alien1.png',
-    './img/alien2.png',
-    './img/alien3.png',
-    './img/alien4.png',
-    './img/alien5.png',
-    './img/boss.png',
-    './img/exp1.png',
-    './img/exp2.png',
-    './img/exp3.png',
-    './img/exp4.png',
-    './img/exp5.png',
-    './img/explosion.wav',
-    './img/explosion2.wav',
-    './img/laser.wav'
+    './spaceship.png',
+    './alien1.png',
+    './alien2.png',
+    './alien3.png',
+    './alien4.png',
+    './alien5.png',
+    './alien_bullet.png',
+    './bullet.png',
+    './boss.png',
+    './bg.png',
+    './explosion.wav',
+    './explosion2.wav',
+    './laser.wav'
 ];
 
-// Install service worker and cache assets
+// Install event - cache assets
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
-                console.log('Opened cache');
                 return cache.addAll(ASSETS_TO_CACHE);
             })
     );
 });
 
-// Activate service worker and clean up old caches
+// Activate event - clean up old caches
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
                     if (cacheName !== CACHE_NAME) {
-                        console.log('Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
@@ -51,37 +44,26 @@ self.addEventListener('activate', (event) => {
     );
 });
 
-// Fetch event handler
+// Fetch event - serve from cache, fall back to network
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
             .then((response) => {
-                // Cache hit - return response
                 if (response) {
                     return response;
                 }
-
-                // Clone the request
-                const fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest).then(
-                    (response) => {
-                        // Check if we received a valid response
+                return fetch(event.request)
+                    .then((response) => {
                         if (!response || response.status !== 200 || response.type !== 'basic') {
                             return response;
                         }
-
-                        // Clone the response
                         const responseToCache = response.clone();
-
                         caches.open(CACHE_NAME)
                             .then((cache) => {
                                 cache.put(event.request, responseToCache);
                             });
-
                         return response;
-                    }
-                );
+                    });
             })
     );
 }); 
